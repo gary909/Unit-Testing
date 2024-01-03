882cf3d1713d960e7cf6ca218de2f1e9692353a5
whg <whg@makrosolutions.com>
2024-01-03 14:13:01 +0100

    testing asynchronous code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 //import logo from './logo.svg';
 import Greeting from './components/Greeting';
 import './App.css';
+import Async from './components/Async';
 
 function App() {
   return (
     <div className="App">
       <Greeting />
+      <br></br>
+      <Async />
     </div>
   );
 }
