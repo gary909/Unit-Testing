357ae4f2c467746e1df223bf5c979896c4da36db
whg <whg@makrosolutions.com>
2024-01-03 12:00:44 +0100

    added test description heading

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,27 +1,31 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
 
-test('to see if page contains hello world text', () => {
-    // Arrange
-    render(<Greeting/>);
-
-    // Act
-    // ... nothing
-
-    // Assert
-    const helloWorldElement = screen.getByText('Hello World!');
-    expect(helloWorldElement).toBeInTheDocument();
+describe('Greeting Component', () => {
+    test('to see if page contains hello world text', () => {
+        // Arrange
+        render(<Greeting/>);
+    
+        // Act
+        // ... nothing
+    
+        // Assert
+        const helloWorldElement = screen.getByText('Hello World!');
+        expect(helloWorldElement).toBeInTheDocument();
+    });
+    
+    // New test case to check if the page contains 'It's good to see you!' text
+    test('to see if page contains good to see you! text', () => {
+        // Arrange
+        render(<Greeting/>);
+    
+        // Act
+        // ... nothing
+    
+        // Assert
+        const greetingTextElement = screen.getByText('Good to see you!');
+        expect(greetingTextElement).toBeInTheDocument();
+    });
 });
 
-// New test case to check if the page contains 'It's good to see you!' text
-test('to see if page contains good to see you! text', () => {
-    // Arrange
-    render(<Greeting/>);
 
-    // Act
-    // ... nothing
-
-    // Assert
-    const greetingTextElement = screen.getByText('Good to see you!');
-    expect(greetingTextElement).toBeInTheDocument();
-});
