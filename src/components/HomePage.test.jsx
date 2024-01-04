6f19c2a9bf0aaa08d74b29f18890b65f9a471f26
whg <whg@makrosolutions.com>
2024-01-04 12:12:09 +0100

    readme

--- src/components/HomePage.test.jsx
+++ src/components/HomePage.test.jsx
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react'; // The testing library
 import { BrowserRouter } from 'react-router-dom'; // Need this to render without errors
 import HomePage from './HomePage'; // The page we'll be testing
 
+// My first test!
 // This simple test searches to see if the text 'dashboard' is present on the page
 
 test('searches if dashboard text is present on page', () => {
